46af611362728fbd917374ea0b59320a8269bdab
Americano <bjg0726@gmail.com>
2014-10-08 13:03:57 +0900

    initial commit

---
+++ Assets/Scripts/CameraVerticalSetting.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraVerticalSetting : MonoBehaviour {
+
+	public float imageWidth = 720.0f;
+	public float imageHeight = 1280.0f;
+
+	void Awake () {
+		Camera.main.orthographicSize = (imageWidth * 0.5f) * (Camera.main.pixelHeight / Camera.main.pixelWidth);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
